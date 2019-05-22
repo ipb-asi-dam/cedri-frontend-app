77d32baa4a55ad3ced2d740e56f5dc973005744c
Augusto Dias <augustodias@alunos.utfpr.edu.br>
2019-05-22 16:28:38 +0100

    arrumando texto das toolbar, e outro pequenos bugs

--- app/src/main/java/com/example/cedri_app/MenuActivity.kt
+++ app/src/main/java/com/example/cedri_app/MenuActivity.kt
@@ -14,10 +14,6 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        // setSupportActionBar(android.support.v7.widget.Toolbar(this))
-
-        val logoutButton = findViewById<ImageButton>(R.id.logoutImageButton)
-
         logoutButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
