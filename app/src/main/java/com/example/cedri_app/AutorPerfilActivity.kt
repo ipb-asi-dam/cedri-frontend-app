018520a962b096953bbf037311830856c522083a
Augusto Dias <augustodias@alunos.utfpr.edu.br>
2019-06-17 16:44:54 +0100

    simples metodo de chamada

--- app/src/main/java/com/example/cedri_app/AutorPerfilActivity.kt
+++ app/src/main/java/com/example/cedri_app/AutorPerfilActivity.kt
@@ -1,12 +1,17 @@
 package com.example.cedri_app
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_autor_perfil.*
+import java.util.jar.Manifest
 
 class AutorPerfilActivity : AppCompatActivity() {
+
+    val REQUEST_PHONE_CALL = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +24,29 @@ class AutorPerfilActivity : AppCompatActivity() {
         }
 
         authorNum.setOnClickListener {
-            val number = "924077163"
-            val intent = Intent(Intent.ACTION_CALL)
-            intent.data = Uri.parse("tel:$number")
-            startActivity(intent)
+
+            // TODO: COLOCAR ALGUM POPUP INDICANDO AO USUARIO Q ELE SERÁ ENCAMINHADO AO APP DE CHAMADAS E SE ELE ACEITAR ELE VAI PARA LIGAÇÃO
+            // SEGUE LINK PARA UMA POSSIVEL SOLUCAO https://www.youtube.com/watch?v=6_kGW103Ue8 ESTA SOLUÇÂO CONSISTE EM COPIAR O NUMERO PARA O APLICATIVO DE DISCAGEM
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), REQUEST_PHONE_CALL)
+            } else {
+                startCall()
+            }
+        }
+    }
+
+    private fun startCall(){
+        val callIntent = Intent(Intent.ACTION_CALL)
+        val number = "913801231"
+
+        callIntent.data = Uri.parse("tel:" + number)
+        startActivity(callIntent)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PHONE_CALL){
+            startCall()
         }
     }
 }
