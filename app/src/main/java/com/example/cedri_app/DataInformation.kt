9dc3611a6b222f5579876a880801f834788e1dc0
Augusto Dias <augustodias@alunos.utfpr.edu.br>
2019-06-24 00:18:08 +0100

    tentando adicionar DB

---
+++ app/src/main/java/com/example/cedri_app/DataInformation.kt
@@ -0,0 +1,14 @@
+package com.example.cedri_app
+
+class DataInformation {
+
+    var token : String = ""
+
+    constructor(token:String){
+        this.token = token
+    }
+
+    constructor(){
+    }
+
+}
--- app/src/main/java/com/example/cedri_app/MainActivity.kt
+++ app/src/main/java/com/example/cedri_app/MainActivity.kt
@@ -15,6 +15,8 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    var myDB = DataBaseHandler(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -70,6 +72,13 @@ class MainActivity : AppCompatActivity() {
                     val intent = Intent(mainAct, MenuActivity::class.java)
                     val token = response?.body()?.getData()?.token
                     intent.putExtra("token", token)
+                    /*
+                    //armazenar no banco o token
+                    var tokenDB : DataInformation = DataInformation(token.toString())
+                    var db : Boolean = myDB.addData(tokenDB)
+                    myDB.close()*/
+
+
                     startActivity(intent)
                     finish()
                 }
